92c718995beb1b213ef1885ab33126d501871c21
Ho1Ai desktop <koekto33309@gmail.com>
2025-03-19 23:26:20 +0000

    divided appending packages and receiving package info. Now appender is in appendPackage.py and dependencies sender is in packageInfo.py. Gonna add something else btw. Thought it is a good idea to add interesting things and parallely learn FastAPI for future projects

--- router/packageInfo.py
+++ router/packageInfo.py
@@ -1,15 +1,7 @@
 from fastapi import APIRouter, HTTPException
 from db_dir.db import fetchPackageInfo, insertPackage
-from pydantic import BaseModel
-from typing import List
 
 router = APIRouter(prefix="/api")
-
-class PackageRequest(BaseModel):
-    name: str
-    version: str
-    dependencies: List[str]
-    key: str
 
 @router.get('/package-info')
 async def get_package_info(name: str):
@@ -19,9 +11,3 @@ async def get_package_info(name: str):
     return dict(pkg)
     #return({'package': name})
     # return(pkglist.get(name, {"error":"package not found"}))
-
-@router.post('/append-package')
-async def append_package(pkg: PackageRequest):
-    if(pkg.key == 'testKey12345'):
-        await insertPackage(pkg.name, pkg.version, pkg.dependencies)
-        return {"message":"Package added succesfully"}
