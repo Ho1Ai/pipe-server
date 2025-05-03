5e7867fa4f4f21d78de95b90960381179b0b60d2
Ho1Ai Arch <koekto33309@gmail.com>
2025-05-03 10:36:51 +0300

    adding some things for backend and client-side. Data transfer. Learning FastAPI+SQLAlchemy stack

--- db_dir/db.py
+++ db_dir/db.py
@@ -20,7 +20,7 @@ async def fetchPackageInfo(name: str):
         async with pool.acquire() as conn:
             return await conn.fetchrow("select * from packagesList where name = $1", name)
 
-async def insertPackage(name: str, version: str, dependencies: list, pkg_type:str, pkg_build_type: str):
+async def insertPackage(name: str, version: str, dependencies: list, pkg_type:str, pkg_build_type: str, pkg_server_name: str):
     pool = await dbConnect()
     async with pool.acquire() as databasewriter:
-        await databasewriter.execute("insert into packagesList (name, version, dependencies, pkg_type, build_type) values ($1, $2, $3, $4, $5)", name, version, dependencies, pkg_type, pkg_build_type)
+        await databasewriter.execute("insert into packagesList (name, version, dependencies, pkg_type, build_type, server_pkg_name) values ($1, $2, $3, $4, $5, $6)", name, version, dependencies, pkg_type, pkg_build_type, pkg_server_name)
--- router/appendPackage.py
+++ router/appendPackage.py
@@ -10,6 +10,7 @@ class PackageRequest(BaseModel):
     key: str
     pkg_type: str
     pkg_build_type: str
+    pkg_server_name: str
 
 router = APIRouter(prefix='/api')
 
@@ -17,7 +18,7 @@ router = APIRouter(prefix='/api')
 async def append_package(pkg: PackageRequest):
     if(pkg.key == 'testKey12345'):
         if(pkg.pkg_type == 'app' or pkg.pkg_type == 'lib'):
-            await insertPackage(pkg.name, pkg.version, pkg.dependencies, pkg.pkg_type, pkg.pkg_build_type)
+            await insertPackage(pkg.name, pkg.version, pkg.dependencies, pkg.pkg_type, pkg.pkg_build_type, pkg.pkg_server_name)
             return {"message": "Package added succesfully"}
         else:
             return {"message": "You can't use this type of package; you can use next types of packages: lib, app"}
